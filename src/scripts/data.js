aad8883f1a5b190a841600af553db7ccbbd87805
Peter Pyshnyy <petyapyshnyy@gmail.com>
2022-04-12 21:24:19 +0200

    habs zurückgetan

---
+++ src/scripts/data.js
@@ -0,0 +1,14 @@
+// export const companies = () =>
+//   fetch("json/companies.json").then((res) => res.json());
+// export const tickets = () =>
+//   fetch("json/tickets.json").then((res) => res.json());
+// export const segments = () =>
+//   fetch("json/segments.json").then((res) => res.json());
+
+// // let ticketsArr = [];
+
+// // tickets().then((data) => {
+// //   data.forEach((element) => {
+// //     ticketsArr.push(element.price);
+// //   });
+// // });
